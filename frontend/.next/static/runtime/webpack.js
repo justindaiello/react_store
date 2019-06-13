a45ea462619c6c93cabd78704f247bb97006c894
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-13 13:15:37 -0400

    Adding CSS to header and nav, adding router and connected NProgress library.

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2cbf374a161281f30a05";
+/******/ 	var hotCurrentHash = "ac619ee1a4c76ed4a200";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
 
 //Need to be strings b/c this is not CSS
 const theme = {
   red: '#FF0000',
   black: '#393939',
   grey: '#3A3A3A',
-  lightGrey: '#E1E1E1',
+  lightgrey: '#E1E1E1',
   offWhite: '#EDEDED',
   maxWidth: '1000px',
   boxShadow: '0 12px 24px 0 rgba(0,0,0,.09)' 
@@ -24,6 +25,40 @@ const Inner = styled.div`
   margin: 0 auto;
   padding: 2rem;
 `;
+
+//inject global css
+injectGlobal`
+  @font-face {
+    font-family: radnika_next;
+    src: url('/static/radnikanext-medium-webfont.woff2')
+    format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    /* make font base 10 */
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: radnika_next;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+
+`
 
 class Page extends Component {
   render() {
