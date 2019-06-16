18fb05471d92238b48862c0f2633f49633cc81f7
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-16 16:36:35 -0400

    Single page render route set up.

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fe1e74f2d72c98486b04";
+/******/ 	var hotCurrentHash = "7ee2d827ac25e6093547";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
