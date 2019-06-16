c4b50c428829f26f754a6c7aff993c7d270857ab
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-16 14:49:02 -0400

    Added update item mutation.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,7 @@
 //resolvers
 
 const mutations = {
+  
   async createItem(parent, args, context, info) {
     //interface with Prisma DB, get access to methods in prisma.graphql file. returns a promise. need to make it async/await for item to go into item value
     const item = await context.db.mutation.createItem({
@@ -11,6 +12,22 @@ const mutations = {
     }, info);
     
     return item
+  },
+
+  updateItem(parent, args, context, info) {
+    //take a copy of the updates
+    const updates = { ...args }
+    //remove ID from the updates do we dont change it for a later
+    delete updates.id;
+    //run the update method
+    return context.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    }, 
+    info //contains the query with item info to be returned
+    );
   }
 };
 
