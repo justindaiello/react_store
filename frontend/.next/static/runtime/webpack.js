953cbad1e93cab3dfe3dfd1c054ee1ea82918929
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-17 10:49:52 -0400

    Styled the sell-form a little bit more.

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0fb4821698fda618081f";
+/******/ 	var hotCurrentHash = "d9ef29f1e377d7cd9960";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
