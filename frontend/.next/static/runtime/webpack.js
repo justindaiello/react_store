ce91ac644fb99647516889434ccdba48d7c7d6e0
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-18 11:44:27 -0400

    Adding some logic to the nav bar to display different info for logged in or logged out users.

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c5d58a8bb34ff67ad8d5";
+/******/ 	var hotCurrentHash = "dc6dd73f67401181fd04";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,30 +4,36 @@ import User from './User';
 import { log } from 'util';
 
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {({ data: { me } }) => {
-        console.log(me)
-        if (me) return <p>{me.name}</p>
-        return null;
-      }}
-    </User>
-    <Link href="/items">  
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">  
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">  
-      <a>Sign-up</a>
-    </Link>
-    <Link href="/orders">  
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">  
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  
+  <User>
+    {({ data: { me } }) => (
+    <NavStyles>
+      <Link href="/items">  
+        <a>Shop</a>
+      </Link>
+      {me && (
+        <React.Fragment>
+          <Link href="/sell">  
+            <a>Sell</a>
+          </Link>
+          <Link href="/orders">  
+            <a>Orders</a>
+          </Link>
+          <Link href="/me">  
+            <a>Account</a>
+          </Link>
+        </React.Fragment>
+      )}
+
+      {!me && (
+        <Link href="/signup">  
+          <a>Sign-In</a>
+        </Link>
+      )}
+    </NavStyles>
+    )}
+  </User>
+    
 )
 
 export default Nav;
