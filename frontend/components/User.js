1119eec26ea005276fef3de042e3f2dc3a7692e0
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-17 20:10:53 -0400

    added custom render prop component to reuse user query.

---
+++ frontend/components/User.js
@@ -0,0 +1,28 @@
+//CUSTOM RENDER PROP COMPONENT 
+// lets us re-use this component without having to write a new query every single time
+
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+
+const CURRENT_USER_QUERY = gql`
+  me {
+    id 
+    email
+    name
+    permissions
+  }
+`;
+
+const User = (props) => (
+  <Query {...props} query={CURRENT_USER_QUERY}>
+    {payload => props.children(payload)}
+  </Query>
+);
+
+User.PropTypes = {
+  children: PropTypes.func.isRequired,
+}
+
+export default User;
+export { CURRENT_USER_QUERY };
