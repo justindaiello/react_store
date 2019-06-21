f0084e5c995128b509105b6b0f1eddb42700af2e
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-20 23:30:43 -0400

    prepping for frontend heroku deployment.

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:3001`;
+export const prodEndpoint = https://goadventure-yoga-prod.herokuapp.com/
 //pagination count
 export const perPage = 6;
