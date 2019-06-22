ea7142c27e5362262ab50b13d659718b0bee104e
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-22 18:50:17 -0400

    Added a re-route to home page when user resets password.

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3f0588f69bc32e1529dd";
+/******/ 	var hotCurrentHash = "99fb2cbcd5da4134ff02";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
