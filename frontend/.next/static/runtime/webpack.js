a43f559c97da7ab802ca6c3f867eff9f3b217373
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-22 16:53:32 -0400

    Adding favicon.

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9402392f5b2021d81b38";
+/******/ 	var hotCurrentHash = "83d08b7339d24a98a4e0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
