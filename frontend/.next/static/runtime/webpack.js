22e14cce69c2eaf2f607b58ad4f6c17af1608f1c
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-27 13:44:32 -0400

    Adding extra args for email client.

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d73ccc67b6c08ef5f63a";
+/******/ 	var hotCurrentHash = "6f60f0d75dc0c06b2efb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
