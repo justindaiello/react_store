381f6f1e7ab045357a0a89e126dc58751fd13886
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-29 15:55:38 -0400

    Setting up files for testing.

---
+++ frontend/__tests__/sample.test.js
@@ -0,0 +1,7 @@
+//just has a handful of globals already available that don't need to be imported
+
+//Describe whats going on and give it a group of related tests
+describe('Sample test', () => {
+  console.log('I am running some tests!');
+  
+})
