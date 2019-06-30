a0af987c96361a7d505138d00f50ccbd71971e87
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-30 18:35:06 -0400

    Initial testing of formatMoney function.

---
+++ frontend/__tests__/formatMoney.test.js
@@ -0,0 +1,7 @@
+import formatMoney from '../lib/formatMoney';
+
+describe('formatMoney Function', () => {
+  it('works with fractional dollars', () => {
+    expect(formatMoney(1)).toEqual('$0.01')
+  })
+})
--- frontend/__tests__/sample.test.js
+++
@@ -1,7 +0,0 @@
-//just has a handful of globals already available that don't need to be imported
-
-//Describe whats going on and give it a group of related tests
-describe('Sample test', () => {
-  console.log('I am running some tests!');
-  
-})
