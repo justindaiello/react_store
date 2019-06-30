71e04f2326387c1152c3499002493c859c54ef1c
Aiello <aiello@aiellos-mbp.attlocal.net>
2019-06-30 18:42:03 -0400

    Jest up and running and formatMoney tested.

--- frontend/__tests__/formatMoney.test.js
+++ frontend/__tests__/formatMoney.test.js
@@ -2,6 +2,20 @@ import formatMoney from '../lib/formatMoney';
 
 describe('formatMoney Function', () => {
   it('works with fractional dollars', () => {
-    expect(formatMoney(1)).toEqual('$0.01')
+    expect(formatMoney(1)).toEqual('$0.01');
+    expect(formatMoney(10)).toEqual('$0.10');
+    expect(formatMoney(43)).toEqual('$0.43');
+  });
+
+  it('Leaves cents off for whole dollars', () => {
+    expect(formatMoney(5000)).toEqual('$50');
+    expect(formatMoney(1000)).toEqual('$10');
+    expect(formatMoney(500000)).toEqual('$5,000');
+  });
+
+  it('Works with whole and factional dollars', () => {
+    expect(formatMoney(5032)).toEqual('$50.32');
+    expect(formatMoney(101)).toEqual('$1.01');
+    expect(formatMoney(333333)).toEqual('$3,333.33');
   })
 })
